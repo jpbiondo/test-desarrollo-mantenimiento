027570098205bb273c42ad36981c51844c19d4c2
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-09-23 22:04:20 -0300

    add test coverage conditions

---
+++ src/tests/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "jest";
+const config: Config = {
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
+
+export default config;
